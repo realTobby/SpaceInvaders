6a87c5895cb978eec051604ef391df9692d7ea43
realTobby <tkattanek00@gmail.com>
2021-06-07 22:23:23 +0200

    build batch
    create build directory before trying to output the .exe

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+mkdir D:\Programmierung\SpaceInvaders\build
 g++ D:\Programmierung\SpaceInvaders\src\*.cpp -o D:\Programmierung\SpaceInvaders\build\spaceinvaders.exe -Iinclude -lsfml-graphics -lsfml-window -lsfml-system
 pause
 start D:\Programmierung\SpaceInvaders\build\spaceinvaders.exe
