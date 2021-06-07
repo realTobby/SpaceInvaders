12de691c24e3d8b97a6bffe2a542dcd98774051e
realTobby <tkattanek00@gmail.com>
2021-06-07 22:14:34 +0200

    Shooting
    Player can shoot, rotated the parallax starfield

--- src/game.cpp
+++ src/game.cpp
@@ -17,36 +17,100 @@ class Game
         sf::Vector2f starLayer2[100];
         sf::Vector2f starLayer3[100];
 
+        sf::Vector2f playerPosition = sf::Vector2f(400.f,500.f);
+
+        bool canShoot = true;
+        sf::Vector2f shotPosition = sf::Vector2f(playerPosition.x, playerPosition.y);
+
         void Starfield()
         {
             // create basic star shape
-            sf::RectangleShape shape(sf::Vector2f(2.f,2.f));
+            sf::RectangleShape shape(sf::Vector2f(1.2f,1.2f));
             shape.setFillColor(sf::Color::White);
 
             for(int i = 0; i < 100; i++)
             {
                 shape.setPosition(starLayer1[i]);
-                starLayer1[i].x = starLayer1[i].x - 0.09f;
+                starLayer1[i].y = starLayer1[i].y - 0.09f;
 
-                if(starLayer1[i].x <= 0)
-                    starLayer1[i].x = 800;
+                if(starLayer1[i].y <= 0)
+                {
+                    starLayer1[i].x = rand() % 800 + 1;
+                    starLayer1[i].y = 600;
+                }
                 mainWindow.draw(shape);
 
                 shape.setPosition(starLayer2[i]);
-                starLayer2[i].x = starLayer2[i].x - 0.06f;
+                starLayer2[i].y = starLayer2[i].y - 0.06f;
 
-                if(starLayer2[i].x <= 0)
-                    starLayer2[i].x = 800;
+                if(starLayer2[i].y <= 0)
+                {
+                    starLayer2[i].x = rand() % 800 + 1;
+                    starLayer2[i].y = 600;
+                }
                 mainWindow.draw(shape);
 
                 shape.setPosition(starLayer3[i]);
-                starLayer3[i].x = starLayer3[i].x - 0.02f;
+                starLayer3[i].y = starLayer3[i].y - 0.02f;
 
-                if(starLayer3[i].x <= 0)
-                    starLayer3[i].x = 800;
+                if(starLayer3[i].y <= 0)
+                {
+                    starLayer3[i].x = rand() % 800 + 1;
+                    starLayer3[i].y = 600;
+                }
                 mainWindow.draw(shape);
             }
 
+        }
+
+        void shoot()
+        {
+            shotPosition.x = playerPosition.x + 16.f;
+            shotPosition.y = playerPosition.y;
+            canShoot = false;
+        }
+
+        void moveShoot()
+        {
+            if(canShoot == false)
+            {
+                shotPosition.y = shotPosition.y - 0.25f;
+                sf::RectangleShape shotShape(sf::Vector2f(10.f,20.f));
+                shotShape.setFillColor(sf::Color::Red);
+                shotShape.setPosition(shotPosition);
+                mainWindow.draw(shotShape);
+
+                if(shotPosition.y < 0)
+                    canShoot = true;
+            }
+        }
+
+        void PlayerMove()
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && playerPosition.x > 0)
+            {
+                // move left...
+                //cout << "Move Left..." << endl;
+                playerPosition.x = playerPosition.x - 0.09f;
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && playerPosition.x < 800-48)
+            {
+                // move right...
+                //cout << "Move Right..." << endl;
+                playerPosition.x = playerPosition.x + 0.09f;
+            }
+            else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && canShoot == true)
+            {
+                shoot();
+            }
+        }
+
+        void DrawPlayer()
+        {
+            sf::RectangleShape playerShape(sf::Vector2f(48.f,48.f));
+            playerShape.setFillColor(sf::Color::Green);
+            playerShape.setPosition(playerPosition);
+            mainWindow.draw(playerShape);
         }
 
 
@@ -86,11 +150,16 @@ class Game
                     {
                         if (event.type == sf::Event::Closed)
                             mainWindow.close();
+
                     }
 
                     mainWindow.clear();
 
                     Starfield();
+                    DrawPlayer();
+                    PlayerMove();
+
+                    moveShoot();
 
                     mainWindow.display();
                 }
